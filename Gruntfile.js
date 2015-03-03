eec5ba45a290701cb88452f37d03c3e5f0a4defe
Gabriel Melo <veggabriel@gmail.com>
2015-03-03 17:05:18 -0300

    Reconstrução da estrutura do arquivo css do grid

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,38 +4,26 @@ module.exports = function(grunt) {
 grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    /* uglify */
-    uglify: {
-        options: {
-            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-        },
-        build: {
-            src: 'src/<%= pkg.name %>.js',
-            dest: 'build/<%= pkg.name %>.min.js'
-        }
-    },
-
     /* sass */
-    sass: {                              // Task
-        dist: {                            // Target
-            options: {                       // Target options
-                style: 'expanded',
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-
-            files: {
-                'css/main.css': 'css/main.scss',
-            }
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'css/demo.css': 'scss/demo.scss',
+          'css/grid.css': 'scss/grid.scss',
         }
+      }
     }
+
 });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+// Load the plugin that provides the "uglify" task.
+grunt.loadNpmTasks('grunt-contrib-sass');
 
-    // Default task(s).
-    grunt.registerTask('default', ['uglify, sass']);
+// Default task(s).
+grunt.registerTask('default', ['sass']);
 
 };
 
---
+++ README.md
@@ -0,0 +1,28 @@
+# GRID SIMPLES
+This document will rescribe the streture of grid simples
+
+---
+
+## Installation 
+* [Bower](http://bower.io/): `bower install --`
+* [Github](http://github.com): `git clone https://github.com`
+* Download:
+
+
+
+## The Grid System
+
+**Description**
+
+- 12 columns
+- Responsive, fluid
+- Columns will be wrapped in a container . row
+- Each column will have the class of .column-$
+- Also creat a .container class that wraps all content and centers it on the page. The container must be fluid in width.
+
+**Responsive**
+
+- in Smaller screen sizes (<1024px) the columns will have their gutter width halved
+- In mobile sizes (<768px) all columns will get a 100% width
+
+**Exmple uses:**
